165def4f6671fce2ff213af15cef95beb3d1c83b
Sviatlana Kisel <kisels1.20@gmail.com>
2021-09-29 14:49:03 +0300

    refactored code

--- task5.1.js
+++ task5.1.js
@@ -5,21 +5,25 @@
 var sum1 = 0;
 var sum2 = 0;
 const maxValue = 6;
+
 function getRundom(max) {
     return Math.floor(Math.random() * max + 1);
+}
+
+function makeDecision(sum1, sum2) {
+    if (sum1 > sum2) {
+        console.log('Player 1 win! Player 1  sum "' + sum1 + '" is greater than Player 2  sum "' + sum2 + '"');
+    }
+    else if (sum1 < sum2) {
+        console.log('Player 2 win! Player 1  sum "' + sum1 + '" is less than Player 2  sum "' + sum2 + '"');
+    }
+    else {
+        console.log('A drawn game');
+    }
 }
 
 for (let i = 3; i > 0; i--) {
     sum1 += getRundom(maxValue);
     sum2 += getRundom(maxValue);
 }
-
-if (sum1 > sum2) {
-    console.log('Player 1 win! Player 1  sum "' + sum1 + '" is greater than Player 2  sum "' + sum2 + '"');
-}
-else if (sum1 < sum2) {
-    console.log('Player 2 win! Player 1  sum "' + sum1 + '" is less than Player 2  sum "' + sum2 + '"');
-}
-else {
-    console.log('A drawn game');
-}
+makeDecision(sum1, sum2);
