7812407a1463df79813349ac8dcbcce0bbd16d31
Sviatlana Kisel <kisels1.20@gmail.com>
2021-09-29 13:30:01 +0300

    added task and formated code

--- task5.1.js
+++ task5.1.js
@@ -1,22 +1,25 @@
-var sum1=0;
-var sum2=0;
+/*
+Эмулировать игру в кубики, 2 человека по очереди бросают кубик, каждый в итоге по 3 раза.
+У кого сумма трех бросков будет наибольшей - тот выиграл. Если суммы равны то ничья
+*/
+var sum1 = 0;
+var sum2 = 0;
 const maxValue = 6;
-function getRundom(max){
+function getRundom(max) {
     return Math.floor(Math.random() * max + 1);
 }
 
-for(let i = 3; i>0;i--){
-    sum1 +=getRundom(maxValue);
-    sum2 +=getRundom(maxValue);
+for (let i = 3; i > 0; i--) {
+    sum1 += getRundom(maxValue);
+    sum2 += getRundom(maxValue);
 }
 
-if(sum1 > sum2){
+if (sum1 > sum2) {
     console.log('Player 1 win! Player 1  sum "' + sum1 + '" is greater than Player 2  sum "' + sum2 + '"');
 }
-else if(sum1 < sum2){
+else if (sum1 < sum2) {
     console.log('Player 2 win! Player 1  sum "' + sum1 + '" is less than Player 2  sum "' + sum2 + '"');
 }
-else{
+else {
     console.log('A drawn game');
 }
-
